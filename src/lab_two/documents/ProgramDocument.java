33a47628707c361c14e4723b557893097e4240a0
Chiuliana <chichioiuliana@gmail.com>
2023-10-27 00:42:59 +0300

    Create subclass ProgramDocument

--- src/lab_two/documents/ProgramDocument.java
+++ src/lab_two/documents/ProgramDocument.java
@@ -1,11 +1,47 @@
 package lab_two.documents;
 
-public abstract class ProgramDocument extends Document {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ProgramDocument extends Document {
+    private int lineCount;
+    private int classCount;
+    private int methodCount;
+
     public ProgramDocument(String filename) {
-        super(filename);
+        super(filename, "py");
+        // Initialize lineCount, classCount, methodCount here
     }
 
-    public abstract int getLineCount();
-    public abstract int getClassCount();
-    public abstract int getMethodCount();
+    @Override
+    public void getInfo() {
+        // Implement logic to get program file information
+    }
+
+    private void calculateProgramStats() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lineCount++;
+                classCount += countOccurrences(line, "\\bclass\\b");
+                methodCount += countOccurrences(line, "\\bdef\\s+(\\w+)\\s*\\(.*\\)\\s*:");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int countOccurrences(String input, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
+    }
 }
+
