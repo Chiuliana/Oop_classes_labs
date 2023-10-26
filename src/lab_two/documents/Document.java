6debb0894f5f54d57aefe31c60f8c0f10ae66353
Chiuliana <chichioiuliana@gmail.com>
2023-10-27 00:39:27 +0300

    Create subclass TextDocument

--- src/lab_two/documents/Document.java
+++ src/lab_two/documents/Document.java
@@ -1,28 +1,20 @@
 package lab_two.documents;
-public abstract class Document {
+// Document.java
+
+import java.util.Date;
+
+public class Document {
     protected String filename;
     protected String extension;
     protected boolean changed;
 
-    public Document(String filename) {
+    public Document(String filename, String extension) {
         this.filename = filename;
-        this.extension = getExtension(filename);
+        this.extension = extension;
         this.changed = false;
     }
 
-    // Getter methods for filename, extension, and changed
-
-    private String getExtension(String filename) {
-        int dotIndex = filename.lastIndexOf('.');
-        if (dotIndex > 0 && dotIndex < filename.length() - 1) {
-            return filename.substring(dotIndex + 1);
-        }
-        return "";
-    }
-
-    public abstract void displayInfo();
-
-    public void displayStatus() {
-        System.out.println(filename + " - " + (changed ? "Changed" : "No Change"));
+    public void getInfo() {
+        // To be implemented in subclasses
     }
 }
--- src/lab_two/documents/TextDocument.java
+++ src/lab_two/documents/TextDocument.java
@@ -1,21 +1,37 @@
 package lab_two.documents;
+// TextDocument.java
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TextDocument extends Document {
     private int lineCount;
     private int wordCount;
     private int charCount;
 
-    public TextDocument(String filename, int lineCount, int wordCount, int charCount) {
-        super(filename);
-        this.lineCount = lineCount;
-        this.wordCount = wordCount;
-        this.charCount = charCount;
+    public TextDocument(String filename) {
+        super(filename, "txt");
+        // Initialize lineCount, wordCount, charCount here
     }
 
     @Override
-    public void displayInfo() {
-        System.out.println("File: " + filename + "." + extension);
-        System.out.println("Line count: " + lineCount);
-        System.out.println("Word count: " + wordCount);
-        System.out.println("Character count: " + charCount);
+    public void getInfo() {
+        // Implement logic to get text file information
+    }
+
+    private void calculateTextStats() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lineCount++;
+                wordCount += line.split("\\s+").length;
+                charCount += line.length();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
