73b8cbe34d50c47f622d7a5d82d4ec7e7d3b4c40
Chiuliana <chichioiuliana@gmail.com>
2023-10-27 14:33:01 +0300

    Create DocumentManagerCLI

---
+++ src/lab_two/monitor/DocumentManagerCLI.java
@@ -0,0 +1,38 @@
+package lab_two.monitor;
+
+import java.util.Scanner;
+
+public class DocumentManagerCLI {
+
+    private DocumentManager documentManager;
+
+    public DocumentManagerCLI(DocumentManager documentManager) {
+        this.documentManager = documentManager;
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("> ");
+            String command = scanner.nextLine();
+
+            if (command.equals("commit")) {
+                documentManager.commit();
+            } else if (command.startsWith("info")) {
+                String[] parts = command.split(" ");
+                if (parts.length != 2) {
+                    System.out.println("Invalid command. Usage: info <filename>");
+                } else {
+                    String filename = parts[1];
+                    documentManager.printFileInfo(filename);
+                }
+            } else if (command.equals("status")) {
+                documentManager.printStatus();
+            } else if (command.equals("exit")) {
+                break;
+            } else {
+                System.out.println("Invalid command. Available commands: commit, info <filename>, status, exit");
+            }
+        }
+    }
+}
