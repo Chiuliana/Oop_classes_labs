4e1e177684213c975b4286a55a3d59eed6934c8f
Chiuliana <chichioiuliana@gmail.com>
2023-11-13 17:14:02 +0200

    create queue class in behaviour

--- src/lab_three/behaviour/Queue.java
+++ src/lab_three/behaviour/Queue.java
@@ -1,8 +1,8 @@
 package lab_three.behaviour;
 
-public interface Stack<T> {
-    void push(T element);
-    T pop();
+public interface Queue<T> {
+    void enqueue(T element);
+    T dequeue();
     T peek();
     boolean isEmpty();
     boolean isFull();
